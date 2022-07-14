e10f5658ebceb29c3d870d85749f3b2b8de82f5e
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-14 18:42:05 -0400

    created and added code to 01-introuction.ts and 02-globals.ts

---
+++ 02-globals.ts
@@ -0,0 +1,18 @@
+// GLOBALS
+
+// __dirname
+// __filename
+// require
+// module
+// process
+
+console.log(__dirname)
+console.log(__filename)
+
+setInterval(() => {
+  console.log('hello world')
+}, 1000);
+
+setTimeout(() => {
+  console.log('hello world 2');
+}, 5000);
