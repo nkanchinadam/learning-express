22938341b101d35d2a1dd138758daf651b626b46
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-14 18:18:48 -0400

    information about globals

--- app.ts
+++ app.ts
@@ -1,10 +1,7 @@
-const amount = 9
+// GLOBALS
 
-if(amount < 10) {
-  console.log('small number');
-}
-else {
-  console.log('large number');
-}
-
-console.log("hey it's my first node app!!!");  
+// __dirname
+// __filename
+// require
+// module
+// process
