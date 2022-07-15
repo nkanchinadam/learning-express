d38a3c3742688f9b39233ac2f0d5f000d16b5ddc
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-15 00:34:27 -0400

    added imports to 03-modules.ts

--- src/03-modules.ts
+++ src/03-modules.ts
@@ -1,6 +1,11 @@
 // CommonJS - every file is module (by default)
 // Modules - Encapsulated code (only share minimum)
 
+const names = require('./03a-names.ts');
+const sayHi = require('./03b-utils.ts');
+
+console.log(names);
+
 sayHi('Susan');
-sayHi(john);
-sayHi(peter);
+sayHi(names.john);
+sayHi(names.peter);
