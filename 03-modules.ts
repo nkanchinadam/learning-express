04ad745113ba9047524738b6631e7b2d0be91e77
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-14 23:42:53 -0400

    moved variables to from 03-modules.ts to helper files

--- 03-modules.ts
+++ 03-modules.ts
@@ -1,15 +1,6 @@
 // CommonJS - every file is module (by default)
 // Modules - Encapsulated code (only share minimum)
 
-const secret = 'SUPER SECRET';
-
-const john = 'john';
-const peter = 'peter';
-
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-}
-
 sayHi('susan');
 sayHi(john);
 sayHi(peter);
---
+++ 03a-names.ts
@@ -0,0 +1,3 @@
+const secret = 'SUPER SECRET';
+const john = 'john';
+const peter = 'peter';
