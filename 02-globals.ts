6b4e8a2c37103f654ac95b38611a843ccdcc0086
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-14 23:53:31 -0400

    added comments to 02-globals.ts

--- 02-globals.ts
+++ 02-globals.ts
@@ -1,18 +1,20 @@
 // GLOBALS
 
-// __dirname
-// __filename
-// require
-// module
-// process
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
 
 console.log(__dirname)
 console.log(__filename)
 
+// setInterval repeats the callback continuously after a set period of time
 setInterval(() => {
   console.log('hello world')
 }, 1000);
 
+// setTImeout executes the callback once after a set period of time
 setTimeout(() => {
   console.log('hello world 2');
 }, 5000);
