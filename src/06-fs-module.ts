56d6a2614b07bac2ff0335854ec61e243c81e8f8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-17 18:40:15 -0400

    created and wrote in 06-fs-module.ts and helper files

---
+++ src/06-fs-module.ts
@@ -0,0 +1,8 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('./src/content/first.txt', 'utf8')
+const second = readFileSync('./src/content/second.txt', 'utf8')
+
+console.log(first);
+
+writeFileSync('./src/content/result.txt', 'Here is the result', { flag: 'a' });
