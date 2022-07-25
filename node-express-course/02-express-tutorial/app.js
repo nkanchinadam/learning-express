4b84b307241fd5fc78e6d85bdfde8d30ac561592
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-25 15:26:47 -0400

    updated app.js to create a basic web server using http module and node

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,42 @@
-console.log('Express Tutorial')
+const http = require('http');
+const {readFileSync} = require('fs');
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method);
+  console.log(req.url);
+
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(homePage);
+  }
+  else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homeStyles);
+  }
+  else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homeImage);
+  }
+  else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homeLogic);
+  }
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>about page</h1>');
+  }
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>page not found</h1>');
+  }
+  res.end();
+});
+
+server.listen(5000);
