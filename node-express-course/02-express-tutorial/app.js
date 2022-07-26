b581c6237008717fd5c7f8bee7a8cfb27be98858
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-26 19:05:19 -0400

    formatting

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const {products, people} = require('./data')
+const { products, people } = require('./data')
 
 app.get('/', (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
@@ -9,35 +9,35 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', (req, res) => {
   const newProducts = products.map((product) => {
-    const {id, name, image} = product;
-    return {id, name, image};
+    const { id, name, image } = product;
+    return { id, name, image };
   })
   res.json(newProducts);
 });
 
 app.get('/api/products/:productID', (req, res) => {
-  const {productID} = req.params;
+  const { productID } = req.params;
   const singleProduct = products.find((product) => product.id === Number(productID));
-  if(!singleProduct) {
+  if (!singleProduct) {
     return res.status(404).send('Product does not exist!');
   }
   return res.json(singleProduct);
 });
 
 app.get('/api/v1/query', (req, res) => {
-  const {search, limit} = req.query;
+  const { search, limit } = req.query;
   let sortedProducts = [...products];
-  if(search) {
+  if (search) {
     sortedProducts = sortedProducts.filter((product) => {
       return product.name.startsWith(search);
     });
   }
-  if(limit) {
+  if (limit) {
     sortedProducts = sortedProducts.slice(0, Number(limit));
   }
-  if(sortedProducts.length === 0) {
+  if (sortedProducts.length === 0) {
     // return res.status(200).send('no products matched your search')
-    return res.status(200).json({success: true, data: []})
+    return res.status(200).json({ success: true, data: [] })
   }
   res.status(200).json(sortedProducts);
 });
