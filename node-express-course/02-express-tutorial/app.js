fcdff6e069c9b68ed0e22199fd9e7cc49d14e3e8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-26 15:30:29 -0400

    added basic api route to retrieve products

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -4,7 +4,15 @@ const app = express();
 const {products, people} = require('./data')
 
 app.get('/', (req, res) => {
-  res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product;
+    return {id, name, image};
+  })
+  res.json(newProducts);
 });
 
 app.listen(5000, () => {
