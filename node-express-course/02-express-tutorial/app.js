befc6ade2e60d2e3a6e7bf2ea670be05a75c3d2f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-26 15:21:39 -0400

    set up a basic API for products in app.js using res.json()

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,32 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// setup static and middleware
-app.use(express.static('./public'));
+const {products, people} = require('./data')
 
-// other options for index.html are adding to static assets & server-side rendering
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// });
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000');
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
