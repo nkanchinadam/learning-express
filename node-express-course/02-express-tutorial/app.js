858fbba5599104dc2a613fa6a18f7af8548f8f0c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-26 11:38:08 -0400

    converted app.js into an express project

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,42 +1,32 @@
-const http = require('http');
-const {readFileSync} = require('fs');
+const express = require('express');
+const path = require('path');
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method);
-  console.log(req.url);
+// setup static and middleware
+app.use(express.static('./public'));
 
-  const url = req.url;
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homePage);
-  }
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' });
-    res.write(homeStyles);
-  }
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' });
-    res.write(homeImage);
-  }
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' });
-    res.write(homeLogic);
-  }
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>about page</h1>');
-  }
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' });
-    res.write('<h1>page not found</h1>');
-  }
-  res.end();
+app.get('/', (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
-server.listen(5000);
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+});
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000');
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
