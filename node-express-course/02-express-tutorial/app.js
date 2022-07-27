5fdd086a8ec4627a68a97f57aa7eccd6ab63bad8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-26 22:28:40 -0400

    moved logger function into logger.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,15 +1,9 @@
 const express = require('express');
+const logger = require('./logger');
+
 const app = express();
 
 // req => middleware => res
-
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-}
 
 app.get('/', logger, (req, res) => {
   res.send('Home');
