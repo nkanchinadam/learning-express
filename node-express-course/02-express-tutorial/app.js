f217913b5f8ba9f72e69a209f2f0ea1aa516a808
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-27 19:55:29 -0400

    created a POST request to handle form data

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -3,8 +3,20 @@ const app = express();
 
 let { people } = require('./data');
 
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send('Please Provide Credentials');
 });
 
 app.listen(5000, () => {
