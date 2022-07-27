772f4ff823f35a05a0abfb124f5c82ff4a21e023
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-27 15:29:00 -0400

    installed morgan package and used it as a middleware function in app.js

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const logger = require('./logger');
 const authorize = require('./authorize');
 
@@ -6,8 +7,12 @@ const app = express();
 
 // req => middleware => res
 
+// 1. use vs route
+// 2. options - our own / express / third party
+
 // app.use calls a middleware function whenever the base of the route matches the path provided
-app.use([logger, authorize]);
+app.use(morgan('tiny'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.send('Home');
