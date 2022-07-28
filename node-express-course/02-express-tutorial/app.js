e0edd73887d12fadcdd83c2cafb3db76d77947e4
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-28 13:03:42 -0400

    added a POST request to '/api/people' that returns the name that the user inputted if it is a valid name

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -3,11 +3,20 @@ const app = express();
 
 let { people } = require('./data');
 
-app.use(express.static('./methods-public'))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.static('./methods-public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if(!name) {
+    return res.status(400).json({success: false, msg: 'please provide name value'});
+  }
+  res.status(201).json({success: true, person: name});
 });
 
 app.post('/login', (req, res) => {
