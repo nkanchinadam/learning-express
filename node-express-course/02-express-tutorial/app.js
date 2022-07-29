2a71e41237b1558066fe06f0e23ce3683936fd1c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-28 22:07:26 -0400

    added a PUT request to '/api/people/:id' to allow user to update people data

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -13,10 +13,10 @@ app.get('/api/people', (req, res) => {
 
 app.post('/api/people', (req, res) => {
   const { name } = req.body;
-  if(!name) {
-    return res.status(400).json({success: false, msg: 'please provide name value'});
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'please provide name value' });
   }
-  res.status(201).json({success: true, person: name});
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post('/login', (req, res) => {
@@ -26,6 +26,26 @@ app.post('/login', (req, res) => {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send('Please Provide Credentials');
+});
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
