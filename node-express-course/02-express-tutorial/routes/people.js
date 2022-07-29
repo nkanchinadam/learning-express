99ac955b6afbb3158d79b0bacc777409edfe92f3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-28 23:21:00 -0400

    added another way of setting up a router

--- node-express-course/02-express-tutorial/routes/people.js
+++ node-express-course/02-express-tutorial/routes/people.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const { getPeople, createPerson, updatePerson, deletePerson } = require('../controllers/people');
 
-router.get('/', getPeople);
-router.post('/', createPerson);
-router.put('/:id', updatePerson);
-router.delete('/:id', deletePerson);
+// router.get('/', getPeople);
+// router.post('/', createPerson);
+// router.put('/:id', updatePerson);
+// router.delete('/:id', deletePerson);
+
+router.route('/').get(getPeople).post(createPerson);
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
 module.exports = router;
