3e94a1b1e0a1e01d1f2ba99976a449c9a6ca13c5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-28 23:13:18 -0400

    moved people.js callback functions from the routes folder to the controllers folder

---
+++ node-express-course/02-express-tutorial/controllers/people.js
@@ -0,0 +1,45 @@
+let { people } = require('../data');
+
+const getPeople = (req, res) => {
+  res.status(200).json({ success: true, data: people });
+}
+
+const createPerson = (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
+}
+
+const updatePerson = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: newPeople });
+}
+
+const deletePerson = (req, res) => {
+  const { id } = req.params;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.filter((person) => person.id !== Number(id));
+  res.status(200).json({ success: true, data: newPeople });
+}
+
+module.exports = { getPeople, createPerson, updatePerson, deletePerson }
