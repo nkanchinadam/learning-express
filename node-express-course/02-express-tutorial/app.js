58ee38aea817db3182c7d65155548e017514a6c6
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2022-07-28 22:29:41 -0400

    added a DELETE request to '/api/peope/:id' to allow users to delete people's data from the server

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -48,6 +48,16 @@ app.put('/api/people/:id', (req, res) => {
   res.status(200).json({ success: true, data: newPeople });
 });
 
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.filter((person) => person.id !== Number(id));
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, () => {
   console.log('server is listening on port 5000');
 });
